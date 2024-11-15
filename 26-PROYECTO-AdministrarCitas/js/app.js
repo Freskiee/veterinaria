6526967a82a837df32a50d5dc9de85cb72f7b585
Ariel Sanchez <freska_96@hotmail.com>
2024-11-15 03:54:06 -0600

    FEAT: Creando una clase para notificación para mostrar mensajes de error

--- 26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -31,11 +31,44 @@ function datosCita(e) {
     citaObj[e.target.name] = e.target.value;
 };
 
+//* Valida que no haya campos vacios
 function submitCita(e) {
     e.preventDefault();
-
     if (Object.values(citaObj).some(valor => valor.trim() === '')) {
-        console.log('todos los campos son obligatorios');
+        new Notificacion({
+            texto: 'Todos los campos son obligatorios.',
+            tipo: 'error',
+        });
         return;
     };
 };
+
+
+class Notificacion {
+
+    constructor({texto, tipo}) {
+        this.texto = texto;
+        this.tipo = tipo;
+
+        this.mostrar();
+    };
+
+    // Esto es un metodo
+    mostrar() {
+        //~ Crear la notificacion
+        const alerta = document.createElement('DIV');
+        alerta.classList.add('text-center', 'w-full', 'p-3', 'text-white', 'my-5', 'alert', 'uppercase', 'font-bold', 'text-sm');
+        
+        //~ Si es de tipo error, agrega una clase
+        this.tipo === 'error' ? alerta.classList.add('bg-red-500') : alerta.classList.add('bg-green-500');
+
+        //~ Mensaje de error
+        alerta.textContent = this.texto;
+
+        //~ Insertar en el DOM
+        formulario.parentElement.insertBefore(alerta, formulario); // El primer elemento es lo que quieres insertar, el segundo donde lo quieres insertar
+    };
+
+};
+
+
