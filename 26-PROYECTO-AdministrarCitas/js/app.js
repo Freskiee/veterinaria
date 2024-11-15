293244326bea0038ad2409088c768ca8de25fc09
Ariel Sanchez <freska_96@hotmail.com>
2024-11-15 04:00:49 -0600

    feat: Evitar Alertas Duplicadas

--- 26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -46,7 +46,7 @@ function submitCita(e) {
 
 class Notificacion {
 
-    constructor({texto, tipo}) {
+    constructor({ texto, tipo }) {
         this.texto = texto;
         this.tipo = tipo;
 
@@ -58,7 +58,11 @@ class Notificacion {
         //~ Crear la notificacion
         const alerta = document.createElement('DIV');
         alerta.classList.add('text-center', 'w-full', 'p-3', 'text-white', 'my-5', 'alert', 'uppercase', 'font-bold', 'text-sm');
-        
+
+        //~ Eliminar las alertas duplicadas
+        const alertaPrevia = document.querySelector('.alert');
+            alertaPrevia?.remove();
+
         //~ Si es de tipo error, agrega una clase
         this.tipo === 'error' ? alerta.classList.add('bg-red-500') : alerta.classList.add('bg-green-500');
 
@@ -67,6 +71,11 @@ class Notificacion {
 
         //~ Insertar en el DOM
         formulario.parentElement.insertBefore(alerta, formulario); // El primer elemento es lo que quieres insertar, el segundo donde lo quieres insertar
+
+        //~ Quitar despues de 3 seg.
+        setTimeout(() => {
+            alerta.remove();
+        }, 3000);
     };
 
 };
